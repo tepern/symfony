57d9cca1f014d9f940cd01ee26e66106f9802c0b
nteper <nteper@codemastersintl.com>
2025-04-02 16:54:52 +0300

    Migration & CRUD

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Blog;
+use App\Repository\BlogRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class DefaultController extends AbstractController
 {
-    #[Route('/default/{id}', name: 'app_default', requirements: ['page' => '\d+'])]
-    public function index(int $id): Response
+    #[Route('/default', name: 'app_default')]
+    public function index(BlogRepository $blogRepository, EntityManagerInterface $em)
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController', 'id' => $id
-        ]);
+        $blog = $blogRepository->findOneBy(['id' => 1]);
+        $blog->setTitle('Test');
+        $blog = (new Blog())
+            ->setTitle('Title')
+            ->setText('Text');
+
+        $em->persist($blog);
+        $em->flush();
+        
+        return $this->render('default/index.html.twig', []);
     }
 }
