f67cf6fb32be6a1aee3410d3de90b366bdb66814
nteper <nteper@codemastersintl.com>
2025-04-28 14:10:37 +0300

    Registration form

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,13 +6,14 @@ use App\Entity\Blog;
 use App\Repository\BlogRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_default')]
-    public function index()
+    public function index(): Response
     {
-        return $this->redirectToRoute('app_blog_index');
+        return new Response();
     }
 }
